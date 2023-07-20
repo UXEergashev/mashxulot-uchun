ee1f5b916a21448d95517c8a8634491dba7103ae
erali <eraliergashev875@gmail.com>
2023-07-20 21:01:35 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//4-mashq
+//Given an int n, return the absolute difference between n and 21,
+// except return double the absolute difference if n is over 21.
+// let n=prompt("n=")
+// if(n<21){
+//     s=21-n;
+//     console.log(s);
+// }
+// else if(n>21){
+//     s=(n-21)*2;
+//     console.log(s)
+// }
+//5-mashq
+//We have a loud talking parrot. The "hour" parameter
+// is the current hour time in the range 0..23. We are in
+//   trouble if the parrot is talking and the hour is before
+//    7 or after 20. Return true if we are in trouble.
+// let a=prompt("a=");
+// if (a < 7 || a > 20) {
+//     console.log(true);
+//   } else {
+//    console.log(false);
+//   }
+
